1487d25680021c9895d74095c80ff9c9c25588f5
fadhilahroy <royfasyafadhilah@gmail.com>
2019-06-03 09:35:56 +0700

    tambah kelas aksesoris

---
+++ src/transportasi/darat/Aksesoris.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package transportasi.darat;
+
+/**
+ *
+ * @author ApemKukus
+ */
+public class Aksesoris {
+    
+    public void aksesoris(){
+        System.out.println("ini adalah kelas aksesoris");
+    }
+}
