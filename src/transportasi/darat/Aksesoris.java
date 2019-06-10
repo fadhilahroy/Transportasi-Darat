93c4d46c94f0a36ba2b10122294e686c3999ba7a
KurniaKhaikal <krnhl29@gmail.com>
2019-06-10 09:13:20 +0700

    Penambahan Encapsulation di class Aksesoris

--- src/transportasi/darat/Aksesoris.java
+++ src/transportasi/darat/Aksesoris.java
@@ -10,8 +10,53 @@ package transportasi.darat;
  * @author ApemKukus
  */
 public class Aksesoris {
-    
-    public void aksesoris(){
-        System.out.println("ini adalah kelas aksesoris");
+
+    public double getiStereo() {
+        return iStereo;
     }
+
+    public void setiStereo(double iStereo) {
+        this.iStereo = iStereo;
+    }
+
+    public double getModified() {
+        return Modified;
+    }
+
+    public void setModified(double Modified) {
+        this.Modified = Modified;
+    }
+
+    public double getCustomised() {
+        return Customised;
+    }
+
+    public void setCustomised(double Customised) {
+        this.Customised = Customised;
+    }
+
+    public double getLeather() {
+        return Leather;
+    }
+
+    public void setLeather(double Leather) {
+        this.Leather = Leather;
+    }
+
+    public double getGPS() {
+        return GPS;
+    }
+
+    public void setGPS(double GPS) {
+        this.GPS = GPS;
+    }
+    
+    
+        private double iStereo = 103.49;
+        private double Modified = 500.78;
+        private double Customised = 2000.49;
+        private double Leather = 1003.90;
+        private double GPS = 180.29;
+
+    
 }
